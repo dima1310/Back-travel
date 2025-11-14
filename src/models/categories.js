520023cac2faca71e71edab48742874d5c00d0f7
Kamila <kamillkaa95@gmail.com>
2025-11-15 01:41:49 +0200

    Update categories schema with trim option
    Added 'trim' option to the 'name' field and removed version key.

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,13 +1,18 @@
 import { model, Schema } from 'mongoose';
 
 const categoriesSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        }
-    }
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { versionKey: false },
 );
 
-export const CategoriesCollection = model('categories', categoriesSchema, 'travellers.categories');
-
+export const CategoriesCollection = model(
+  'categories',
+  categoriesSchema,
+  'travellers.categories',
+);
